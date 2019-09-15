c31b5071b4592d75bfc20719062e60ad0d2e7fb1
Micah Levine <micah.levine@outlook.com>
2019-09-15 00:15:52 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
 # Add your code here
+class Dog
+
+@@all = []
+
+attr_accessor  :name
+
+
+def initialize(name)
+  @name = name
+  @@all << self
+end
+
+def self.all
+  @@all.each do |dog|
+    puts dog.name
+  end
+end
+
+def self.clear_all
+  @all.clear
+end
+
+def clear
+
+end
+
+
+end
+end
